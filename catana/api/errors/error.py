b7f41492b0de2967742094bcf4a193cc4ea75417
MRmlik12 <mrmlik12sup@gmail.com>
2021-07-13 10:10:43 +0200

    Add new unit tests

--- catana/api/errors/error.py
+++ catana/api/errors/error.py
@@ -2,5 +2,6 @@ from fastapi import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 
+
 async def http_error_handler(_: Request, exc: HTTPException) -> JSONResponse:
-    return JSONResponse({"error": [exc.detail]}, exc.status_code)
+    return JSONResponse({"errors": [exc.detail]}, exc.status_code)
