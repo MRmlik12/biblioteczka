9c54252256746b94035dd0661c83f61c37e38158
MRmlik12 <mrmlik12sup@gmail.com>
2021-07-19 12:22:38 +0200

    Add fixture to create book record

--- test/test_api/test_routes/test_book.py
+++ test/test_api/test_routes/test_book.py
@@ -1,11 +1,33 @@
 import pytest
 from fastapi import FastAPI
 from httpx import AsyncClient
+from sqlalchemy.sql.expression import table
 from starlette.status import HTTP_200_OK
 from catana.services.token import generate_token
+from catana.core.config import POSTGRESQL_CONNECTION_STRING
+from uuid import UUID
+import sqlalchemy
 import json
 
 pytestmark = pytest.mark.asyncio
+
+
+@pytest.fixture
+async def add_example_records():
+    example_book = {
+        "id": UUID("1b5ffaca-1623-44f5-a055-41a01feaaf9d"),
+        "title": "Pan Tadeusz",
+        "author": "Adam Mickiewicz",
+        "isbn": "9788304004467",
+        "category": "Poetry",
+        "publishing": "Hippocrene Books",
+        "is_borrowed": "false",
+        "user_borrow_id": None,
+        "updated_at": None,
+    }
+    engine = sqlalchemy.create_engine(POSTGRESQL_CONNECTION_STRING).connect()
+    engine.execute("INSERT INTO books(id, title, author, isbn, category, publishing, is_borrowed, user_borrow_id, updated_at)", 
+    (UUID("1b5ffaca-1623-44f5-a055-41a01feaaf9d"), "Pan Tadeusz", "Adam Mickiewicz", "9788304004467", "Poetry", "Hippocrene Books", False, None, None))
 
 
 async def test_book_list_check_status_code_is_OK(app: FastAPI, client: AsyncClient):
