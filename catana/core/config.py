329e4a20d2b7f0e734bed203c49a76eb65e47e57
MRmlik12 <mrmlik12sup@gmail.com>
2021-07-20 08:17:50 +0200

    Fix config to get env

--- catana/core/config.py
+++ catana/core/config.py
@@ -1,8 +1,4 @@
 import os
 
-from starlette import config
-from starlette.config import Config
-
-conf = Config(".env")
-POSTGRESQL_CONNECTION_STRING = "postgresql://test:123@localhost:5432/catana_db"
-JWT_SECRET_KEY = "OPdNtRVFv/lJ+sPQyvy/UDoPe4w3cE4Ld8DKWXK1hLGOoKzfyWkDVdqmwuGT2dbwyrSp5i9HOifgoKpRDpV5kg=="
+POSTGRESQL_CONNECTION_STRING = os.getenv("POSTGRESQL_CONNECTION_STRING")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
