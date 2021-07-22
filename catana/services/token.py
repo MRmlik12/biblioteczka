227fe48558326b68f7f453ff9a6253efa4068853
MRmlik12 <mrmlik12sup@gmail.com>
2021-07-22 11:50:24 +0200

    Fix code style

--- catana/services/token.py
+++ catana/services/token.py
@@ -16,13 +16,13 @@ def generate_token(email: str) -> str:
     """Generates JWT"""
     expires = datetime.utcnow() + timedelta(TOKEN_EXPIRE_IN_MINUTES)
     jwt_user = JWTUser(email=email, exp=expires, sub=SUBJECT)
-    return jwt.encode(jwt_user.dict(), JWT_SECRET_KEY, ALGORITHM)
+    return jwt.encode(jwt_user.dict(), str(JWT_SECRET_KEY), ALGORITHM)
 
 
 def get_email_from_token(token: str) -> str:
     """Try to get email from JWT"""
     try:
-        decode = jwt.decode(token, JWT_SECRET_KEY, list(ALGORITHM))
+        decode = jwt.decode(token, str(JWT_SECRET_KEY), list(ALGORITHM))
         return decode["email"]
     except jwt.PyJWTError as jwt_error:
         raise ValueError("unable to decode JWT token") from jwt_error
